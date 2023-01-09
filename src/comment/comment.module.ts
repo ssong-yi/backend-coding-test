40fb7b2eefb225b8a98b653504dcad3e43954ebe
csy <csl8804@gmail.com>
2023-01-10 03:36:25 +0900

    feat: comment 기본 세팅

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comment } from './entities/comment.entity';
+import { CommentResolver } from './comment.resolver';
+import { CommentService } from './comment.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Comment])],
+  exports: [TypeOrmModule],
+  providers: [CommentResolver, CommentService],
+})
 export class CommentModule {}
