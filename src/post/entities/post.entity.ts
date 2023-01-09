31164ccf385c984f1297593d188b127a3408202a
csy <csl8804@gmail.com>
2023-01-10 03:33:18 +0900

    refactor: id 타입 string으로 변경
    - id 타입을 number에서 string으로 변경
    - 수정일자 컬럼 추가

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -3,20 +3,21 @@ import {
   Entity,
   PrimaryGeneratedColumn,
   CreateDateColumn,
+  UpdateDateColumn,
   DeleteDateColumn,
   OneToMany,
   ManyToOne,
 } from 'typeorm';
-import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { User } from 'src/user/entities/user.entity';
-import { Comment } from 'src/comment/comment.entity';
+import { Comment } from 'src/comment/entities/comment.entity';
 
 @ObjectType()
 @Entity('POST')
 export class Post {
-  @Field(() => Int)
-  @PrimaryGeneratedColumn()
-  id: number;
+  @Field()
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Field()
   @Column({
@@ -33,6 +34,9 @@ export class Post {
 
   @CreateDateColumn()
   createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 
   @DeleteDateColumn()
   deletedAt: Date;
