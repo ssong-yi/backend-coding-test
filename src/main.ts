916e3c081e2d08b16bf342a7e18b23ce165fb73b
csy <csl8804@gmail.com>
2023-01-11 00:10:02 +0900

    refactor: 환경변수를 이용해 port 설정 및 로깅 처리

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -7,6 +9,9 @@ async function bootstrap() {
     logger: ['error', 'warn', 'log'],
   });
 
-  await app.listen(4000);
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get('PORT') || 4000);
+  Logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
